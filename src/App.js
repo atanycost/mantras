ea48224fe8f71f4d264eb8350e70bdd4babcd610
MehraAnkit <110310312+MehraAnkit@users.noreply.github.com>
2024-10-17 14:18:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HanumanChalisa from './Components/HanumanChalisa';
+
+function App() {
+  return (
+    <div >
+      <HanumanChalisa/>
+    </div>
+  );
+}
+
+export default App;
